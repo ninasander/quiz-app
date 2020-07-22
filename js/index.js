439f4b5f0e4357e6c31df1688b35da3c93bc413d
Nina Sander <nina.sander224@outlook.de>
2020-07-22 16:32:44 +0200

    Compile all html to index and add page display js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
 "use strict";
 
-console.log('it works!');
-console.log('hello world');
+var buttonIndex = document.querySelector('.nav__home');
+var buttonBookmarks = document.querySelector('.nav__bookmarks');
+var buttonCreate = document.querySelector('.nav__create');
+var buttonProfile = document.querySelector('.nav__profile');
+var pageIndex = document.querySelector('.page__index');
+var pageBookmarks = document.querySelector('.page__bookmarks');
+var pageCreate = document.querySelector('.page__create');
+var pageProfile = document.querySelector('.page__profile'); // buttonNav.addEventListener('click', () => {
+//     pageBookmarks.classList.add('hidden')
+//     pageCreate.classList.add('hidden')
+//     pageProfile.classList.add('hidden')
+//     pageIndex.classList.remove('hidden')
+//   })
+
+buttonIndex.addEventListener('click', function () {
+  pageBookmarks.classList.add('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.add('hidden');
+  pageIndex.classList.remove('hidden');
+});
+buttonBookmarks.addEventListener('click', function () {
+  pageBookmarks.classList.remove('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.add('hidden');
+  pageIndex.classList.add('hidden');
+});
+buttonCreate.addEventListener('click', function () {
+  pageBookmarks.classList.add('hidden');
+  pageCreate.classList.remove('hidden');
+  pageProfile.classList.add('hidden');
+  pageIndex.classList.add('hidden');
+});
+buttonProfile.addEventListener('click', function () {
+  pageBookmarks.classList.add('hidden');
+  pageCreate.classList.add('hidden');
+  pageProfile.classList.remove('hidden');
+  pageIndex.classList.add('hidden');
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,43 @@
-console.log('it works!')
-console.log('hello world')
+const buttonIndex = document.querySelector('.nav__home')
+const buttonBookmarks = document.querySelector('.nav__bookmarks')
+const buttonCreate = document.querySelector('.nav__create')
+const buttonProfile = document.querySelector('.nav__profile')
+const pageIndex = document.querySelector('.page__index')
+const pageBookmarks = document.querySelector('.page__bookmarks')
+const pageCreate = document.querySelector('.page__create')
+const pageProfile = document.querySelector('.page__profile')
+
+// buttonNav.addEventListener('click', () => {
+//     pageBookmarks.classList.add('hidden')
+//     pageCreate.classList.add('hidden')
+//     pageProfile.classList.add('hidden')
+//     pageIndex.classList.remove('hidden')
+//   })
+
+buttonIndex.addEventListener('click', () => {
+  pageBookmarks.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+  pageIndex.classList.remove('hidden')
+})
+
+buttonBookmarks.addEventListener('click', () => {
+  pageBookmarks.classList.remove('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.add('hidden')
+  pageIndex.classList.add('hidden')
+})
+
+buttonCreate.addEventListener('click', () => {
+  pageBookmarks.classList.add('hidden')
+  pageCreate.classList.remove('hidden')
+  pageProfile.classList.add('hidden')
+  pageIndex.classList.add('hidden')
+})
+
+buttonProfile.addEventListener('click', () => {
+  pageBookmarks.classList.add('hidden')
+  pageCreate.classList.add('hidden')
+  pageProfile.classList.remove('hidden')
+  pageIndex.classList.add('hidden')
+})
